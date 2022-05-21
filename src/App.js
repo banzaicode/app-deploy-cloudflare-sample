f8fcaa15a6584d38185c5bc1533880bcff71a70c
Banzai Code <banzaicode@gmail.com>
2022-05-21 20:31:02 -0300

    landing change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App with CD in cloudflare from github.
         </p>
         <a
           className="App-link"
