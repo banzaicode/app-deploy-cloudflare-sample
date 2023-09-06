a8d0f6a06ac33eae8245d8903c04ab1557b91609
RockBanzai <hrocca76@gmail.com>
2023-09-06 10:24:17 -0300

    Test de prueba 2023

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App with CD in cloudflare from github. Con Gus y Andres.
+          App with CD in cloudflare from github. Con Gus y Andres. 2023
         </p>
         <a
           className="App-link"
